427f12769903b3990c6e6273fe15aa4c76a6d8af
凯笑 张 <1692577337@qq.com>
2022-11-23 23:32:49 +0800

    11.23

---
+++ Code_CStudy/Code_CStudy/test11.23.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//
+//int main() {
+//
+//	printf("hello world");
+//	return 0;
+//}
+
+
+
+//int main() {
+//	int age = 20;
+//	age = age + 1;
+//	printf("%d", age);
+//// %d  -  ÕûĞÍ
+//// %f  -  float
+//// %lf  -  double
+//	return 0;
+//}
+ 
+
+//int main() {
+//	int a = 0;
+//	int b = 0;
+//	scanf(" %d %d", &a, &b);
+//
+//	int sum = a + b;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+int main() {
+	char arr1[] = "hello";
+	char arr2[] = { 'h', 'e', 'l','l', '0' };
+	char arr3[] = { 'h', 'e', 'l','l', '0', '\0' };
+	printf("%s\n", arr1); 
+	printf("%s\n", arr2); 
+	printf("%s\n", arr3); 
+	return 0;
+
+}
